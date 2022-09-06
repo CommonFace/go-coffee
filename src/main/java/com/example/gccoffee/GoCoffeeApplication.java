a504c1699d9dc57099d38d08e655bc1735c03f7e
CommonFace <wlsdlf0605@gmail.com>
2022-09-06 13:47:20 +0900

    Initial commit

---
+++ src/main/java/com/example/gccoffee/GoCoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.example.gccoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoCoffeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoCoffeeApplication.class, args);
+    }
+
+}
